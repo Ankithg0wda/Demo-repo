4e1e7f5760922e3e5a6419e95a3a767e825a3c26
Ankith A <ankithg88@gmail.com>
2024-02-12 01:32:29 +0530

    global.js file is added

---
+++ global.js
@@ -0,0 +1,11 @@
+const john = 'jonh'
+const peter = 'peter'
+
+const sayHi = (name) => {
+ //console.log(`Hello there `,name) 
+ console.log(`sum is ${name}`)
+}
+
+sayHi('ankith')
+sayHi(john)
+sayHi(peter)
