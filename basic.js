4b00ffc1fadd7745256d0a3a925eefe22543ac4a
Ankith A <ankithg88@gmail.com>
2024-02-12 00:58:02 +0530

    added basic.js file

---
+++ basic.js
@@ -0,0 +1,17 @@
+const a=50
+const b=40
+
+function add()
+{
+    const c=a+b
+    console.log('sum of a and b is ',c)
+}
+
+function sub()
+{
+    const d= a-b
+    console.log('sub of a and b is ',d)
+}
+
+add()
+sub()
