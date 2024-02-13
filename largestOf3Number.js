317a2bb8b67c23519fdba08a430ff553aa9da70f
Ankith A <ankithg88@gmail.com>
2024-02-13 23:17:23 +0530

    program to largest of 3 number

---
+++ largestOf3Number.js
@@ -0,0 +1,15 @@
+const num1=20,num2=10,num3=50;
+
+if(num1>num2 && num1>num3)
+{
+    console.log('Largest no is '+num1)
+}
+else if(num2>num3 && num2>num1)
+{
+    console.log('Largest number is '+num2)
+}
+
+else
+{
+    console.log('Largest number is '+num3)
+}
